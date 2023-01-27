2643315e47ea2491ac3ff02e63d4a6a4f815da82
Cheetah <Cheetah@cheetahs-mbp.home>
2023-01-27 23:08:21 +0100

    Test change

--- simple.py
+++ simple.py
@@ -6,3 +6,14 @@ Created on Fri Jan 27 22:52:30 2023
 @author: Cheetah
 """
 
+def range_of_list(x):
+    """
+    Accepts a list
+    
+    Returns
+    -------
+    Difference between its max and min.
+
+    """
+    return max(x) - min(x)
+
