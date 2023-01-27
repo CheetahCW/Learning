cbdf96b8ec6733766a067659fec8ff07bfaf4951
Cheetah <Cheetah@cheetahs-mbp.home>
2023-01-28 00:41:04 +0100

    Another try

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 27 22:52:17 2023
+Created on Sat Jan 28 00:14:36 2023
 
 @author: Cheetah
 
 Learning Python.
 """
 
+from learning import functions
+from learning import mydice
