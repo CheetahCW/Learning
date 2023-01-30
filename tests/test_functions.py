8490b862a671dcf9a62f322e2ef6c82da8ac27f5
Cheetah <Cheetah@cheetahs-mbp.home>
2023-01-30 02:20:40 +0100

    Recent updates

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,7 +4,7 @@ Tests for learning/functions/
 
 """
 
+from learning.functions.simple import range_of_list
 
-import learning.functions.simple as fs
-
- 
+def test_range_of_list():
+    assert range_of_list([-1, -10, 2, 5, 0]) == 15
--- use_case.py
+++ use_case.py
@@ -7,37 +7,24 @@ Created on Fri Jan 27 23:12:19 2023
 """
 
 #import learning.functions.simple as fs
-import learning.mydice as dc
+import learning.random_gen as rg
 #import numpy as np
 #import random
-import time
 
-start_time = time.time()
-#x = [-1, 2, 11, -7, 3, 0]
-#y = fs.range_of_list(x)
 
-dice = dc.RandomGen()
-#z = dice.next_num()
+in_nums = [10, 20, 30, 40, 50, 60]
+in_probs = [.1, .2, .3, .2, .15, .05]
 
-#inprobs = np.array([.1, .2, .3, .4])
-#vals = np.array([10, 20, 30, 40])
-#inprobs_cum = np.cumsum(inprobs)
-#inprobs_cum_new = np.insert(inprobs_cum, 0, 0)
+my_rnd = rg.RandomGen(in_nums, in_probs)
 
-#Alist = []
 total = 10000
-#for i in range(0, total):
-#    idx = np.where(inprobs_cum <= random.random())
-#    idx = np.digitize(random.random(), inprobs_cum_new, True)
-#    val_found = vals[len(*idx)]
-#    val_found = vals[idx - 1]
-#    val_found = dice.next_num()
-#    Alist.append(val_found)
 
-Alist = [dice.next_num() for _ in range(total)]
-outprobs = {x: Alist.count(x)/total for x in Alist}
+Alist = [my_rnd.next_num() for _ in range(total)]
+out_probs = {x: Alist.count(x)/total for x in Alist}
 
-#x = random.random()
-#idx_new = np.digitize(x, inprobs_cum_new, True)
-#val_new = vals[idx_new - 1]
-print("--- %s seconds ---" % (time.time() - start_time))
+in_dict = {in_nums[i]: in_probs[i] for i in range(len(in_nums))}
+for num in out_probs.keys():
+    out_probs[num] = [out_probs[num]]
+    out_probs[num].append(in_dict[num])
+    
+print(out_probs)
